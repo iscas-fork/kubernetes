17aedfecf3f4e86cdb8589a9ff695096de7be028
syswu <wuheng@iscas.ac.cn>
2024-08-12 17:26:24 +0800

    delete needless 'api' versions

--- staging/src/k8s.io/api/extensions/v1beta1/types.go
+++ staging/src/k8s.io/api/extensions/v1beta1/types.go
@@ -17,7 +17,7 @@ limitations under the License.
 package v1beta1
 
 import (
-	appsv1beta1 "k8s.io/api/apps/v1beta1"
+	appsv1beta1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
